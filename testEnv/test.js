b63fdc178c3d305377da23d6e8ff7379a5ce0139
Vincent Kreuzen <vjc.kreuzen@avans.nl>
2024-02-21 16:59:48 +0100

    made a js grid

--- testEnv/test.js
+++ testEnv/test.js
@@ -2,5 +2,19 @@
 
 
 window.onload = (event) => {
-    console.log("page is fully loaded");
-  };
+  console.log("page is fully loaded");
+
+
+
+  for (let i = 1; i <= 100; i++) {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.innerHTML = `<p>${i}</p>`;
+    document.getElementById('grid').appendChild(cell);
+  }
+
+
+
+
+
+};
