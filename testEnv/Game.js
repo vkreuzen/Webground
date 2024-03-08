dbe4fb9a14605233ff9ea946be54558860329846
Vincent <vjc.kreuzen@avans.nl>
2024-03-08 14:53:48 +0100

    fixed submission game

--- testEnv/Game.js
+++ testEnv/Game.js
@@ -24,33 +24,14 @@ export class Game {
 
     async submitBoard(board) {
         console.log(`${this.host}/game/${this.gameId}/board/${this.playerName}}`);
-        return await (await this.send_post(`${this.host}/game/${this.gameId}/board/${this.playerName}}`, { "board": board })).json();
+        return await (await this.send_post(`${this.host}/game/${this.gameId}/board/${this.playerName}`, { "board": board })).json();
     }
 
     async shoot(x, y) {
-        return await (await this.send_post(`${this.host}/game/${this.gameId}/player/${this.playerName}}/shot`, {
+        return await (await this.send_post(`${this.host}/game/${this.gameId}/player/${this.playerName}/shot`, {
             "x": x,
             "y": y
         })).json();
-    }
-
-
-    async barrageAllFields() {
-
-
-        //Step 5 - Shoot all the squares till we win, starting at 1, 1
-        let counter = 0;
-        for (let x = 1; x < 10; x++) {
-            for (let y = 1; y < 10; y++) {
-                response = await send_post(`${host}/game/${gameId}/player/linksonder/shot`, { x, y })
-                let shoot_result = await response.json();
-                counter++;
-                if (shoot_result.winner) {
-                    console.log("We won!");
-                    x = 10; y = 10; //to finish game
-                }
-            }
-        }
     }
     
     async send_post(url, body) {
